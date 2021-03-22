bdf664373fd46e9b4a3ef4098ed52b80bb620922
yonathan <yonathan.cedillos@gmail.com>
2021-03-22 16:19:01 -0600

    error

--- format-validation.sh
+++ format-validation.sh
@@ -2,6 +2,7 @@
 
 if [ `terraform fmt | wc -c` -ne 0 ]
   then
-    echo "You need to format your files"
+    echo "You need to format your files. Run terraform fmt to solve your issues";
+    exit 1;
 fi
 
