5fa62e9536ca544814caaf41b09274cbcb7ae90b
yonathan <yonathan.cedillos@gmail.com>
2021-03-22 16:10:24 -0600

    encrypting files 6

--- format-validation.sh
+++ format-validation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ 'terraform fmt | wc -c' -ne 0 ]
+if [ `terraform fmt | wc -c` -ne 0 ]
   then
     echo "You need to format your files"
 fi
--- vars.tf
+++ vars.tf
@@ -15,12 +15,12 @@ variable "network_cidr" {
 }
 
 variable "tableau_machine_type" {
-  type = string
+  type        = string
   description = "In the form of custom-CPUS-MEM, number of CPUs and memory for custom machine. https://cloud.google.com/compute/docs/instances/creating-instance-with-custom-machine-type#specifications"
 }
 
 variable "tableau_disk_size" {
-  type = number
+  type        = number
   description = "The size of the image in gigabytes."
 }
 
